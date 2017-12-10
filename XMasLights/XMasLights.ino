c5e9c9f8d27888173143888feebe5a1d8c6ed757
Aaron Cammarata <aaron.cammarata@gmail.com>
2017-12-09 17:11:08 -0800

    Add project files.

---
+++ XMasLights/XMasLights.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		XMasLights.ino
+ Created:	12/9/2017 5:11:06 PM
+ Author:	pounc
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
